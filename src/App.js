3644600fd0f00e11f653546f688490159cd8b03f
Web26 <rahul.patel@moontechnolabs.com>
2023-09-19 17:29:32 +0530

    Fixed bug

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Update the import here
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // Update the import here
 
-import Header from './components/Header';
-import Home from './components/Home';
-import Login from './components/Login';
-import Register from './components/Register';
-import Products from './components/Products';
-import ProductDetail from './components/ProductDetails';
-import ContactUs from './components/ContactUs';
-import { Footer } from './components/Footer';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import Profile from './components/Profile';
-import Cart from './components/Cart';
-import Checkout from './components/Checkout';
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Products from "./components/Products";
+import ProductDetail from "./components/ProductDetails";
+import ContactUs from "./components/ContactUs";
+import { Footer } from "./components/Footer";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Profile from "./components/Profile";
+import Cart from "./components/Cart";
+import Checkout from "./components/Checkout";
 
 const App = () => {
-
   return (
-    <Router>
-      <div>
+    <>
       <ToastContainer
         position="top-right"
         autoClose={3000}
@@ -31,23 +29,26 @@ const App = () => {
         draggable
         pauseOnHover
         theme="light"
-        />
+      />
       <ToastContainer />
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/products/:id" element={<ProductDetail />} />
-          <Route path="/contact-us" element={<ContactUs />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/checkout" element={<Checkout />} />
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
+      <Router>
+        <div>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/products/:id" element={<ProductDetail />} />
+            <Route path="/contact-us" element={<ContactUs />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/checkout" element={<Checkout />} />
+          </Routes>
+          <Footer />
+        </div>
+      </Router>
+    </>
   );
 };
 
